3eb4d9bd2a26201b0877c6db1aed8e3f26357708
More <surmore@ptc.com>
2025-01-28 23:35:33 +0530

    I have added new controller

---
+++ WebAPI_Project/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI_Project.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string GetHello()
+        {
+            return " Hello ";
+        }
+    }
+}
